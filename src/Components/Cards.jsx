d0c8265acc65a85dece562e6949d9a3bf17cc36d
Aditya Raj <adi.adityakohli2005@gmail.com>
2024-08-08 05:14:58 +0530

    user navigation feature provided

--- src/Components/Cards.jsx
+++ src/Components/Cards.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 function Cards({ ...props }) {
   return (
-    <div className="w-screen p-2">
-      <div className="bg-gray-100 shadow-md rounded-lg p-2 pr-5 mb-2 transform transition-transform duration-300 hover:cursor-pointer mr-4">
-        <div className="flex flex-col sm:flex-row items-center justify-between text-lg text-gray-800">
-          <div className="flex items-center mb-2 sm:mb-0">
-            <span className="ml-2">{props.userid}</span>
-          </div>
-          <div className="flex items-center mb-2 sm:mb-0">
-            <span className="ml-2">{props.rating}</span>
-          </div>
-          <div className="flex items-center mb-2 sm:mb-0">
-            <span className="ml-2">{props.level}</span>
-          </div>
-          <div className="hidden sm:flex items-center">
-            <span className="ml-2">{props.name}</span>
+    <Link to={`/user/${props.email}`}>
+      <div className="w-screen p-2">
+        <div className="bg-gray-100 shadow-md rounded-lg p-2 pr-5 mb-2 transform transition-transform duration-300 hover:cursor-pointer mr-4">
+          <div className="flex flex-col sm:flex-row items-center justify-between text-lg text-gray-800">
+            <div className="flex items-center mb-2 sm:mb-0">
+              <span className="ml-2">{props.userid}</span>
+            </div>
+            <div className="flex items-center mb-2 sm:mb-0">
+              <span className="ml-2">{props.rating}</span>
+            </div>
+            <div className="flex items-center mb-2 sm:mb-0">
+              <span className="ml-2">{props.level}</span>
+            </div>
+            <div className="hidden sm:flex items-center">
+              <span className="ml-2">{props.name}</span>
+            </div>
           </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
